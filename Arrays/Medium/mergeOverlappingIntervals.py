2c6036484f59901edafd8a637f718b6cb5787005
Gursifath Bhasin <gursifath@users.noreply.github.com>
2022-06-26 22:05:35 -0400

    added one

---
+++ Arrays/Medium/mergeOverlappingIntervals.py
@@ -0,0 +1,14 @@
+def mergeOverlappingIntervals(intervals):
+    mergedIntervals = []
+    intervals.sort()
+    currentInterval = intervals[0]
+    idx = 1
+    while idx < len(intervals):
+        if currentInterval[1] >= intervals[idx][0]:
+            currentInterval[1] = max(intervals[idx][1], currentInterval[1])
+        else:
+            mergedIntervals.append(currentInterval)
+            currentInterval = intervals[idx]
+        idx += 1
+    mergedIntervals.append(currentInterval)
+    return mergedIntervals
